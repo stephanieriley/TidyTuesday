31040ae3e6ee03f5342cbf68e463fde2f0a3b662
stephanieriley <stephanieriley333@gmail.com>
2023-02-01 23:01:34 +0000

    find out which time of day has most movement

--- 2023_week05/2023_week03.R
+++ 2023_week05/2023_week03.R
@@ -4,7 +4,7 @@
 ######################################################
 
 #Packages
-library(tidyverse); library(png); library(grid); library(showtext); library(ggshadow)
+library(tidyverse); library(png); library(grid); library(showtext); library(ggshadow); library(lubridate); library(hms); library(ggtext)
 
 #Read in data
 tuesdata <- tidytuesdayR::tt_load(2023, week = 5)
@@ -23,8 +23,10 @@ unique(cats_uk_reference$tag_id)
 unique(cats_uk_reference$animal_taxon)
 
 
-
-#Plot speed against prey caught to see whether faster cats catch more prey
+######################################################
+#Plot speed against prey caught to see whether faster 
+#cats catch more prey
+######################################################
 #Firstly we will find the average speed of each cat
 cat_speed<- cats_uk %>%
   group_by(tag_id) %>%
@@ -80,6 +82,61 @@ p<- ggplot(cat_speed_prey, aes(x = prey_p_month, y = meanspeed)) +
            size = 14)
 
 ggsave("2023_week05/speed_prey.png", p, dpi = 300)
+
+
+
+######################################################
+#Look at when cats are moving
+######################################################
+#Need to get data in correct format
+#Want to look at number of movement within each hour on each day of week
+cats_movement<- cats_uk %>%
+  mutate(date = as.Date(timestamp), #Collect date
+         day = wday(date, label = T, abbr = F), #Collect weekday
+         timehms = hms::hms(hours = hour(timestamp)), #Collect time
+         hour= factor(format(lubridate::parse_date_time(timehms, c("HMS", "HM")), "%H:%M")), #Collect time rounded to nearest hour
+         ) %>%
+  group_by(day, hour) %>%
+  count()
+
+#x axis labels
+xlabs<- rep("", 24)
+xlabs[c(1,7,13,19,24)]<- as.character(cats_movement$hour[c(1,7,13,19,24)])
+
+
+#Plot
+p2<- ggplot(cats_movement, aes(y = day, x = hour, fill = n)) +
+  geom_tile(color = "#ffffff",
+            lwd = 1,
+            linetype = 1) +
+  coord_equal() +
+  scale_x_discrete(labels = xlabs) + #Looks too busy with all times
+  scale_y_discrete(limits = rev) +
+  scale_fill_gradient(low = "white", high = "#124559") +
+  labs(title = "When are cats the least and <span style='color:#124559;'>most</span> busy?",
+       caption = "@stephril3y
+       Data from Kays et al. (doi: 10.1111/acv.12563) via TidyTuesday") +
+  theme(axis.text.x = element_text(angle = 90, vjust=0.5),
+        axis.title = element_blank(),
+        axis.ticks = element_blank(),
+        plot.background = element_rect(fill = "black", colour = "black"),
+        panel.background = element_rect(fill = "black"),
+        legend.position = "none",
+        plot.title = element_markdown(colour = "white", 
+                                  hjust = 1.5,
+                                  size = 20),
+        axis.text = element_text(colour = "white",
+                            family = "nunsans",
+                            size = 16),
+        plot.caption = element_text(colour = "white",
+                                    family = "nunsans",
+                                    size = 12),
+        plot.margin = margin(0.5,0.5,0.5,0.5, "cm"),
+        axis.title.x = element_text(margin = margin(0,0,0.5,0, "cm"))
+        )
+
+ggsave("2023_week05/cats_movement.png", p2, dpi = 300)
+
 
 
 
